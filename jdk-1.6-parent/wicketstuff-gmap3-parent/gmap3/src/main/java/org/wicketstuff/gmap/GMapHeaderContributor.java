3c0b52a903d1f0fcc9151b6aa514bccb7c5663d9
jteura <jteura@piti.pf>
2013-10-23 10:53:57 -1000

    Ajout de la clé gmap

--- jdk-1.6-parent/wicketstuff-gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/GMapHeaderContributor.java
+++ jdk-1.6-parent/wicketstuff-gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/GMapHeaderContributor.java
@@ -7,84 +7,113 @@ import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WicketEventReference;
 
 public class GMapHeaderContributor extends AbstractHeaderContributor {
-	/** Serial ID. */
-	private static final long serialVersionUID = 1L;
-	// URL for Google Maps' API endpoint.
-	private static final String GMAP_API_URL = "%s://maps.google.com/maps/api/js?v=3&amp;sensor=%s";
-	/** HTTP scheme. */
-	private static final String HTTP = "http";
-	/** Scheme to use (http or https). */
-	private final String scheme;
-	/** using sensor? */
-	private String sensor = "false";
-	/** header contributer. */
-	private IHeaderContributor headerContributor = null;
+    /** Serial ID. */
+    private static final long serialVersionUID = 1L;
+    // URL for Google Maps' API endpoint.
+    private static final String GMAP_API_URL = "%s://maps.google.com/maps/api/js?v=3&amp;sensor=%s%s";
+    /** HTTP scheme. */
+    private static final String HTTP = "http";
+    /** Scheme to use (http or https). */
+    private final String scheme;
+    /** Key gmap. */
+    private final String key;
+    /** using sensor? */
+    private String sensor = "false";
+    /** header contributer. */
+    private IHeaderContributor headerContributor = null;
 
-	public GMapHeaderContributor() {
-		this(HTTP, false);
-	}
+    public GMapHeaderContributor() {
+        this(HTTP, false, null);
+    }
 
-	public GMapHeaderContributor(final boolean sensor) {
-		this(HTTP, sensor);
-	}
+    public GMapHeaderContributor(final boolean sensor) {
+        this(HTTP, sensor, null);
+    }
 
-	public GMapHeaderContributor(final String scheme) {
-		this(scheme, false);
-	}
+    public GMapHeaderContributor(final String scheme) {
+        this(scheme, false, null);
+    }
 
-	/**
-	 * Constructor.
-	 * 
-	 * Should be added to the page.
-	 * 
-	 * @param scheme http or https?
-	 * @param sensor
-	 */
-	public GMapHeaderContributor(final String scheme, final boolean sensor) {
-		this.scheme = scheme;
-		if (sensor) {
-			this.sensor = "true";
-		}
+    public GMapHeaderContributor(final boolean sensor, final String key) {
+        this(HTTP, sensor, key);
+    }
 
-		headerContributor = new IHeaderContributor() {
-			private static final long serialVersionUID = 1L;
+    public GMapHeaderContributor(final String scheme, final String key) {
+        this(scheme, false, key);
+    }
 
-			/**
-			 * @see org.apache.wicket.markup.html.IHeaderContributor#renderHead(org.apache.wicket.markup.html.IHeaderResponse)
-			 */
-			@Override
-			public void renderHead(final IHeaderResponse response) {
-				// les wicket
-				response.renderJavascriptReference(WicketEventReference.INSTANCE);
-				response.renderJavascriptReference(WicketAjaxReference.INSTANCE);
-				// gmap
-				response.renderJavascriptReference(String.format(GMAP_API_URL, scheme, sensor));
-				// gmap/wicket
-				response.renderJavascriptReference(WicketGMapJsReference.INSTANCE);
-			}
-		};
-	}
+    /**
+     * Constructor.
+     * 
+     * Should be added to the page.
+     * 
+     * @param scheme http or https?
+     * @param sensor
+     */
+    public GMapHeaderContributor(final String scheme, final boolean sensor, final String key) {
+        this.scheme = scheme;
+        this.key = key;
+        if (sensor) {
+            this.sensor = "true";
+        }
 
-	public String getSensor() {
-		return sensor;
-	}
+        headerContributor = new IHeaderContributor() {
+            private static final long serialVersionUID = 1L;
 
-	@Override
-	public IHeaderContributor[] getHeaderContributors() {
-		return new IHeaderContributor[] { headerContributor };
-	}
+            /**
+             * @see org.apache.wicket.markup.html.IHeaderContributor#renderHead(org.apache.wicket.markup.html.IHeaderResponse)
+             */
+            @Override
+            public void renderHead(final IHeaderResponse response) {
+                // les wicket
+                response.renderJavascriptReference(WicketEventReference.INSTANCE);
+                response.renderJavascriptReference(WicketAjaxReference.INSTANCE);
+                // gmap
+                if (key == null || key.isEmpty()) {
+                    response.renderJavascriptReference(String.format(GMAP_API_URL, scheme, sensor, ""));
+                } else {
+                    response.renderJavascriptReference(String.format(GMAP_API_URL, scheme, sensor, "&amp;key=" + key));
+                }
+                // gmap/wicket
+                response.renderJavascriptReference(WicketGMapJsReference.INSTANCE);
+            }
+        };
+    }
 
-	/**
-	 * @return the headerContributor
-	 */
-	protected final IHeaderContributor getHeaderContributor() {
-		return headerContributor;
-	}
+    public String getSensor() {
+        return sensor;
+    }
 
-	/**
-	 * @param varHeaderContributor the headerContributor to set
-	 */
-	protected final void setHeaderContributor(final IHeaderContributor varHeaderContributor) {
-		headerContributor = varHeaderContributor;
-	}
+    /**
+     * @return the scheme
+     */
+    public String getScheme() {
+        return scheme;
+    }
+
+    /**
+     * @return the key
+     */
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public IHeaderContributor[] getHeaderContributors() {
+        return new IHeaderContributor[] { headerContributor };
+    }
+
+    /**
+     * @return the headerContributor
+     */
+    protected final IHeaderContributor getHeaderContributor() {
+        return headerContributor;
+    }
+
+    /**
+     * @param varHeaderContributor the headerContributor to set
+     */
+    protected final void setHeaderContributor(final IHeaderContributor varHeaderContributor) {
+        headerContributor = varHeaderContributor;
+    }
 }
