9dda886751ad4b1fdb48bd645f12df1fc8e21c54
jteura <jteura@piti.pf>
2013-10-25 08:28:11 -1000

    Ajout de la clé gmap

--- jdk-1.6-parent/wicketstuff-gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/GMapHeaderContributor.java
+++ jdk-1.6-parent/wicketstuff-gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/GMapHeaderContributor.java
@@ -8,55 +8,92 @@ import org.apache.wicket.markup.html.WicketEventReference;
 
 public class GMapHeaderContributor extends Behavior {
 
-	private static final long serialVersionUID = 1L;
-	// URL for Google Maps' API endpoint.
-	private static final String GMAP_API_URL = "%s://maps.google.com/maps/api/js?v=3&amp;sensor=%s";
-	private static final String HTTP = "http";
-	// We have some custom Javascript.
-	private final String scheme;
-	private String sensor = "false";
+    private static final long serialVersionUID = 1L;
+    // URL for Google Maps' API endpoint.
+    private static final String GMAP_API_URL = "%s://maps.google.com/maps/api/js?v=3&sensor=%s%s";
+    private static final String HTTP = "http";
+    // We have some custom Javascript.
+    private final String scheme;
+    /** Key gmap. */
+    private String key;
+    /** using sensor? */
+    private String sensor = "false";
 
-	public GMapHeaderContributor() {
-		this(HTTP, false);
-	}
+    public GMapHeaderContributor() {
+        this(HTTP, false, null);
+    }
 
-	public GMapHeaderContributor(final boolean sensor) {
-		this(HTTP, sensor);
-	}
+    public GMapHeaderContributor(final boolean sensor) {
+        this(HTTP, sensor, null);
+    }
 
-	public GMapHeaderContributor(final String scheme) {
-		this(scheme, false);
-	}
+    public GMapHeaderContributor(final String scheme) {
+        this(scheme, false, null);
+    }
 
-	/**
-	 * Constructor.
-	 * 
-	 * Should be added to the page.
-	 * 
-	 * @param scheme http or https?
-	 * @param sensor
-	 */
-	public GMapHeaderContributor(final String scheme, final boolean sensor) {
-		this.scheme = scheme;
-		if (sensor) {
-			this.sensor = "true";
-		}
-	}
+    public GMapHeaderContributor(final boolean sensor, final String key) {
+        this(HTTP, sensor, key);
+    }
 
-	@Override
-	public void renderHead(final Component component, final IHeaderResponse response) {
-		super.renderHead(component, response);
-		// les wicket
-		response.renderJavaScriptReference(WicketEventReference.INSTANCE);
-		response.renderJavaScriptReference(WicketAjaxReference.INSTANCE);
-		// gmap
-		response.renderJavaScriptReference(String.format(GMAP_API_URL, scheme, sensor));
-		// gmap/wicket
-		response.renderJavaScriptReference(WicketGMapJsReference.INSTANCE);
-	}
+    public GMapHeaderContributor(final String scheme, final String key) {
+        this(scheme, false, key);
+    }
 
-	public String getSensor() {
-		return sensor;
-	}
+    /**
+     * Constructor.
+     * 
+     * Should be added to the page.
+     * 
+     * @param scheme http or https?
+     * @param sensor
+     */
+    public GMapHeaderContributor(final String scheme, final boolean sensor, final String key) {
+        this.scheme = scheme;
+        this.key = key;
+        if (sensor) {
+            this.sensor = "true";
+        }
+    }
+
+    @Override
+    public void renderHead(final Component component, final IHeaderResponse response) {
+        super.renderHead(component, response);
+        // les wicket
+        response.renderJavaScriptReference(WicketEventReference.INSTANCE);
+        response.renderJavaScriptReference(WicketAjaxReference.INSTANCE);
+        // gmap
+        if (key == null || key.isEmpty()) {
+            response.renderJavaScriptReference(String.format(GMAP_API_URL, scheme, sensor, ""));
+        } else {
+            response.renderJavaScriptReference(String.format(GMAP_API_URL, scheme, sensor, "&key=" + key));
+        }
+        // gmap/wicket
+        response.renderJavaScriptReference(WicketGMapJsReference.INSTANCE);
+    }
+
+    public String getSensor() {
+        return sensor;
+    }
+
+    /**
+     * @return the scheme
+     */
+    public String getScheme() {
+        return scheme;
+    }
+
+    /**
+     * @return the key
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * @param varKey the key to set
+     */
+    public void setKey(final String varKey) {
+        key = varKey;
+    }
 
 }
